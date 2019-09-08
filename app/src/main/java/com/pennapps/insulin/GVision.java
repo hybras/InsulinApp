d9dd0fdde57ee85e82032a0233dc202d81d3e9ec
srnazng <31111505+srnazng@users.noreply.github.com>
2019-09-08 01:13:42 -0400

    Camera?

--- app/src/main/java/com/pennapps/insulin/GVision.java
+++ app/src/main/java/com/pennapps/insulin/GVision.java
@@ -16,46 +16,139 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+//public class GVision {
+//
+//    public static void main(String... args) throws Exception {
+//        // Instantiates a client
+//        try (ImageAnnotatorClient vision = ImageAnnotatorClient.create()) {
+//
+//            String fileName = "./resources/wakeupcat.jpg";
+//
+//            // Reads the image file into memory
+//            Path path = Paths.get(fileName);
+//            byte[] data = Files.readAllBytes(path);
+//            ByteString imgBytes = ByteString.copyFrom(data);
+//
+//            // Builds the image annotation request
+//            List<AnnotateImageRequest> requests = new ArrayList<>();
+//            Image img = Image.newBuilder().setContent(imgBytes).build();
+//            Feature feat = Feature.newBuilder().setType(Type.LABEL_DETECTION).build();
+//            AnnotateImageRequest request = AnnotateImageRequest.newBuilder()
+//                    .addFeatures(feat)
+//                    .setImage(img)
+//                    .build();
+//            requests.add(request);
+//
+//            // Performs label detection on the image file
+//            BatchAnnotateImagesResponse response = vision.batchAnnotateImages(requests);
+//            List<AnnotateImageResponse> responses = response.getResponsesList();
+//
+//            for (AnnotateImageResponse res : responses) {
+//                if (res.hasError()) {
+//                    System.out.printf("Error: %s\n", res.getError().getMessage());
+//                    return;
+//                }
+//
+//                for (EntityAnnotation annotation : res.getLabelAnnotationsList()) {
+//                    annotation.getAllFields().forEach((k,v) -> {
+//                        System.out.printf("%s : %s\n", k, v.toString());
+//                    });
+//                }
+//            }
+//        }
+//    }
+
+
+//}
+
+import com.google.api.gax.longrunning.OperationFuture;
+//import com.google.cloud.storage.Blob;
+//import com.google.cloud.storage.Bucket;
+//import com.google.cloud.storage.Storage;
+//import com.google.cloud.storage.Storage.BlobListOption;
+//import com.google.cloud.storage.StorageOptions;
+import com.google.cloud.vision.v1.AnnotateFileResponse;
+import com.google.cloud.vision.v1.AnnotateFileResponse.Builder;
+import com.google.cloud.vision.v1.AnnotateImageRequest;
+import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.AsyncAnnotateFileRequest;
+import com.google.cloud.vision.v1.AsyncAnnotateFileResponse;
+import com.google.cloud.vision.v1.AsyncBatchAnnotateFilesResponse;
+import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
+import com.google.cloud.vision.v1.Block;
+import com.google.cloud.vision.v1.ColorInfo;
+import com.google.cloud.vision.v1.CropHint;
+import com.google.cloud.vision.v1.CropHintsAnnotation;
+import com.google.cloud.vision.v1.DominantColorsAnnotation;
+import com.google.cloud.vision.v1.EntityAnnotation;
+import com.google.cloud.vision.v1.FaceAnnotation;
+import com.google.cloud.vision.v1.Feature;
+import com.google.cloud.vision.v1.Feature.Type;
+import com.google.cloud.vision.v1.GcsDestination;
+import com.google.cloud.vision.v1.GcsSource;
+import com.google.cloud.vision.v1.Image;
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
+import com.google.cloud.vision.v1.ImageContext;
+import com.google.cloud.vision.v1.ImageSource;
+import com.google.cloud.vision.v1.InputConfig;
+import com.google.cloud.vision.v1.LocalizedObjectAnnotation;
+import com.google.cloud.vision.v1.LocationInfo;
+import com.google.cloud.vision.v1.OperationMetadata;
+import com.google.cloud.vision.v1.OutputConfig;
+import com.google.cloud.vision.v1.Page;
+import com.google.cloud.vision.v1.Paragraph;
+import com.google.cloud.vision.v1.SafeSearchAnnotation;
+import com.google.cloud.vision.v1.Symbol;
+import com.google.cloud.vision.v1.TextAnnotation;
+import com.google.cloud.vision.v1.WebDetection;
+import com.google.cloud.vision.v1.WebDetection.WebEntity;
+import com.google.cloud.vision.v1.WebDetection.WebImage;
+import com.google.cloud.vision.v1.WebDetection.WebLabel;
+import com.google.cloud.vision.v1.WebDetection.WebPage;
+import com.google.cloud.vision.v1.WebDetectionParams;
+import com.google.cloud.vision.v1.Word;
+
+import com.google.protobuf.ByteString;
+
+import com.google.protobuf.util.JsonFormat;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class GVision {
+    public static void detectLabels(String filePath, PrintStream out) throws Exception, IOException
+    {
+        List<AnnotateImageRequest> requests = new ArrayList<>();
 
-    public static void main(String... args) throws Exception {
-        // Instantiates a client
-        try (ImageAnnotatorClient vision = ImageAnnotatorClient.create()) {
+        ByteString imgBytes = ByteString.readFrom(new FileInputStream(filePath));
 
-            String fileName = "./resources/wakeupcat.jpg";
+        Image img = Image.newBuilder().setContent(imgBytes).build();
+        Feature feat = Feature.newBuilder().setType(Type.LABEL_DETECTION).build();
+        AnnotateImageRequest request =
+                AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
+        requests.add(request);
 
-            // Reads the image file into memory
-            Path path = Paths.get(fileName);
-            byte[] data = Files.readAllBytes(path);
-            ByteString imgBytes = ByteString.copyFrom(data);
-
-            // Builds the image annotation request
-            List<AnnotateImageRequest> requests = new ArrayList<>();
-            Image img = Image.newBuilder().setContent(imgBytes).build();
-            Feature feat = Feature.newBuilder().setType(Type.LABEL_DETECTION).build();
-            AnnotateImageRequest request = AnnotateImageRequest.newBuilder()
-                    .addFeatures(feat)
-                    .setImage(img)
-                    .build();
-            requests.add(request);
-
-            // Performs label detection on the image file
-            BatchAnnotateImagesResponse response = vision.batchAnnotateImages(requests);
+        try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
+            BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
             List<AnnotateImageResponse> responses = response.getResponsesList();
 
             for (AnnotateImageResponse res : responses) {
                 if (res.hasError()) {
-                    System.out.printf("Error: %s\n", res.getError().getMessage());
+                    out.printf("Error: %s\n", res.getError().getMessage());
                     return;
                 }
 
+                // For full list of available annotations, see http://g.co/cloud/vision/docs
                 for (EntityAnnotation annotation : res.getLabelAnnotationsList()) {
-                    annotation.getAllFields().forEach((k,v) -> {
-                        System.out.printf("%s : %s\n", k, v.toString());
-                    });
+                    annotation.getAllFields().forEach((k, v) -> out.printf("%s : %s\n", k, v.toString()));
                 }
             }
         }
-    }
+}}
 
-}
